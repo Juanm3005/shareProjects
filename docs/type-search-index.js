4c7431869d3155788c893673cec223c3a19339d4
Juanm3005 <160181133+Juanm3005@users.noreply.github.com>
2025-05-22 22:00:33 -0500

    Delete docs directory

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"model","l":"Controller"},{"p":"ui","l":"Executable"},{"p":"model","l":"HardwareReport"},{"p":"model","l":"LevelSeverity"},{"p":"model","l":"Report"},{"p":"model","l":"SoftwareReport"}];updateSearchResults();
